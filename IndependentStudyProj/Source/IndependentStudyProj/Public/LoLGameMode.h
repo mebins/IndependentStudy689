b648444ffe5b5b523a26107290abe615c1069e81
Mebin Skaria <mebins@gmail.com>
2024-06-18 21:32:52 -0700

    Added Restart Logic, Added Nexus, Added Victory/Defeat Logic, Added Victory/Defeat Screen, Added Target only when other structures destroyed

---
+++ IndependentStudyProj/Source/IndependentStudyProj/Public/LoLGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LoLGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INDEPENDENTSTUDYPROJ_API ALoLGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
